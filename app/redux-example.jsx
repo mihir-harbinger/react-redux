f283aec904de456ad46703f58b6b5901e6e239eb
Mihir Karandikar <Mihir@Mihir-Mac-mini.local>
2016-05-27 13:44:47 +0530

    added support for location fetching

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,4 +1,5 @@
 var redux = require('redux');
+var axios = require('axios');
 
 console.log('Starting redux example');
 
@@ -90,13 +91,56 @@ var removeMovie = (id) => {
   }
 }
 
+//Map reducer and action generators
+//----------------------------------
+var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
+  switch(action.type){
+    case 'START_LOCATION_FETCH':
+      return {
+        isFetching: true,
+        url: undefined
+      };
+    case 'COMPLETE_LOCATION_FETCH':
+      return {
+        isFetching: false,
+        url: action.url
+      };
+    default:
+      return state;
+  }
+};
+
+var startLocationFetch = () => {
+  return {
+    type: 'START_LOCATION_FETCH'
+  };
+};
+
+var completeLocationFetch = (url) => {
+  return {
+    type: 'COMPLETE_LOCATION_FETCH',
+    url: url
+  };
+};
+
+var fetchLocation = () => {
+  store.dispatch(startLocationFetch());
+
+  axios.get('http://ipinfo.io')
+  .then((res) => {
+    var loc = res.data.loc;
+    var baseUrl = 'http://maps.google.com?q=';
+    store.dispatch(completeLocationFetch(baseUrl + loc));
+  });
+}
 
 //Reducer
 //----------------------------------
 var reducer = redux.combineReducers({
   name: nameReducer,
   hobbies: hobbiesReducer,
-  movies: moviesReducer
+  movies: moviesReducer,
+  map: mapReducer
 });
 
 var store = redux.createStore(reducer, redux.compose(window.devToolsExtension ? window.devToolsExtension() : f => f));
@@ -105,8 +149,15 @@ var store = redux.createStore(reducer, redux.compose(window.devToolsExtension ? 
 var unsubscribe = store.subscribe(() => {
   var state = store.getState();
   console.log(state);
+  if(state.map.isFetching){
+    document.getElementById('app').innerHTML = 'Loading...';
+  }
+  else if(state.map.url){
+    document.getElementById('app').innerHTML = '<a href="' + state.map.url + '" target="_blank">Your location</a>';
+  }
 });
 
+fetchLocation();
 store.dispatch(changeName('Mihir'));
 store.dispatch(addHobby('Singing'));
 store.dispatch(removeHobby(1));
